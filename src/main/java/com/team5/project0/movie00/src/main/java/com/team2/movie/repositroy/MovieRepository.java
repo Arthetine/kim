7cbd57e0865fd7670462c0cc717ae6f6c142953e
Arthetine <143064707+Arthetine@users.noreply.github.com>
2023-10-04 17:04:04 +0900

    Update MovieRepository.java

--- src/main/java/com/team5/project0/movie00/src/main/java/com/team2/movie/repositroy/MovieRepository.java
+++ src/main/java/com/team5/project0/movie00/src/main/java/com/team2/movie/repositroy/MovieRepository.java
@@ -10,15 +10,17 @@ import com.team2.movie.DTO.MovieDTO;
 @Repository
 public interface MovieRepository  {
 	
-	List<MovieDTO> getAllMovieList();	
+    List<MovieDTO> getAllMovieList();	
 
     boolean existsByRating(Code rating);
 
     void deleteByMovieId(Long movieId);
     
-    //랭크 리스트도 필요한rk>
+    List<MovieDTO> getRankList();    
+    List<MovieDTO> getMovieListByCategory(String category);
+    List<MovieDTO> getMovieListByFilter(Map<String, List<String>> filter);
+    MovieDTO getBookById(String movie);
+    void setUpdateMovie(MovieDTO movie);
+    void setDeleteMovie(String movieID);      
     
-    List<MovieDTO> getRankList();  
-
-
 }
